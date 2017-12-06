5e6b1760516ac9f2914c163cf87db346bb441a77
Parth Prajapati <parth17prajapati@gmail.com>
2017-12-06 22:00:00 +0530

    Configured react

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: __dirname + '/src/index.js',
+
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: 'bundle.min.js',
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['env']
+            }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+        "process.env": { 
+            NODE_ENV: JSON.stringify("production") 
+        }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+          warnings: false,
+      },
+      output: {
+          comments: false,
+      },
+    })
+  ]
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  devtool: 'inline-source-map',
+  
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: 'bundle.js',
+    libraryTarget: 'umd'
+  },
+
+  devServer: {
+    contentBase: __dirname,
+    compress: true,
+    port: 9000,
+    host: 'localhost',
+    open: true
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['env']
+            }
+        }
+      }
+    ]
+  }
+}
